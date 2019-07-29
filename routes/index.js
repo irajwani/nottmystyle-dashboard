85d74b64ea84989304ceb62d4a3363a5f4fcd9f5
Imad Rajwani <imadrajwani@gmail.com>
2019-07-29 19:41:12 +0500

    MAJOR. Get user emails, need to get product data. Question: Is delivery status from itemsSold the correct source of truth?

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,72 @@ var admin = require('firebase-admin');
 //   res.render('index', { title: 'NottMyStyle Dashboard' });
 // });
 
+function countOrdersFrom(users) {
+  let count = 0;
+  Object.values(users).forEach( user => {
+    if(user.notifications) {
+      if(user.notifications.itemsSold) {
+        Object.values(user.notifications.itemsSold).forEach( order => {
+          console.log('Increment Count');
+          count++
+        })
+      }
+    }
+  })
+
+
+  return count;
+}
+
 module.exports = {
   getOrders: (req, res) => {
-    admin.database().ref('/Users').once('value', (snapshot) => {
-      let users = snapshot.val(), orders = [], currentNotificationsObject;
+    console.log('Begin Order Retrieval')
+    admin.database().ref('/Users').once('value', async (snapshot) => {
+      let users = snapshot.val(), orders = [], obj, buyerRecord, sellerRecord, product;
+      // console.log('Retrieved Users:');
+      let countOfOrders = await countOrdersFrom(users)
+      console.log(countOfOrders);
       Object.values(users).forEach((user, index) => {
 
         if(user.notifications) {
           // console.log('yeah')
           if(user.notifications.itemsSold) {
-            Object.values(user.notifications.itemsSold).forEach(item => {
-              orders.push(item);
+            var productKeys = Object.keys(user.notifications.itemsSold);
+            Object.values(user.notifications.itemsSold).forEach(async (item, index) => {
+              // admin.auth().getUser(item.buyerId).then(userRecord => console.log(userRecord))
+              buyerRecord = await admin.auth().getUser(item.buyerId)
+              sellerRecord = await admin.auth().getUser(item.sellerId)
+              // product =  users[item.sellerId].products[productKeys[index]]
+              
+              var buyerEmail = await buyerRecord.email, sellerEmail = await sellerRecord.email;
+              
+              obj = await {
+                buyerName: item.buyerName,
+                buyerEmail,
+                sellerName: item.sellerName,
+                sellerEmail,
+                uri: item.uri,
+                deliveryStatus: item.deliveryStatus,
+
+              }
+              // console.log(obj);
+              await orders.push(obj);
+              console.log(orders)
+              if(orders.length == countOfOrders) {
+                res.render('index.ejs', {
+                  title: "NottMyStyle Dashboard", orders
+                });
+              }
+              
             })
           }
         }
 
-        if(index == Object.values(users).length - 1) {
-          res.render('index.ejs', {
-            title: "NottMyStyle Dashboard", orders
-          });
-        }
+        // if(index == Object.values(users).length - 1) {
+        //   res.render('index.ejs', {
+        //     title: "NottMyStyle Dashboard", orders
+        //   });
+        // }
 
       })
     })
