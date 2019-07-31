03815bcaafcf7d6be42632c2e232991839674abf
Imad Rajwani <imadrajwani@gmail.com>
2019-07-31 21:22:54 +0500

    ERROR. Cant show product data for specific order. MINOR. Began set up to show products page.

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ function countOrdersFrom(users) {
     if(user.notifications) {
       if(user.notifications.itemsSold) {
         Object.values(user.notifications.itemsSold).forEach( order => {
-          console.log('Increment Count');
+          // console.log('Increment Count');
           count++
         })
       }
@@ -27,12 +27,16 @@ function countOrdersFrom(users) {
 
 module.exports = {
   getOrders: (req, res) => {
-    console.log('Begin Order Retrieval')
-    admin.database().ref('/Users').once('value', async (snapshot) => {
-      let users = snapshot.val(), orders = [], obj, buyerRecord, sellerRecord, product;
+    // console.log('Begin Order Retrieval')
+    admin.database().ref().once('value', async (snapshot) => {
+      let d = snapshot.val();
+      let users = d.Users, products = d.Products, 
+      orders = [], obj, 
+      buyerRecord, sellerRecord;
+      var productName, productPrice, productPostPrice;
       // console.log('Retrieved Users:');
       let countOfOrders = await countOrdersFrom(users)
-      console.log(countOfOrders);
+      // console.log(countOfOrders);
       Object.values(users).forEach((user, index) => {
 
         if(user.notifications) {
@@ -40,9 +44,24 @@ module.exports = {
           if(user.notifications.itemsSold) {
             var productKeys = Object.keys(user.notifications.itemsSold);
             Object.values(user.notifications.itemsSold).forEach(async (item, index) => {
+              let currentProductId = await productKeys[index];
+              
+              if(products[currentProductId] !== undefined) {
+                console.log('The Product Exists', index)
+                productPrice = await products[currentProductId].text.price
+                productPostPrice = await products[currentProductId].text.post_price
+                productName = await products[currentProductId].text.name
+              }
+              else {
+                productPrice = await '-';
+                productPostPrice = await '-';
+                productName = await 'Does not exist in database';
+              }
+              console.log(productPrice, productPostPrice, productName);
               // admin.auth().getUser(item.buyerId).then(userRecord => console.log(userRecord))
               buyerRecord = await admin.auth().getUser(item.buyerId)
               sellerRecord = await admin.auth().getUser(item.sellerId)
+              
               // product =  users[item.sellerId].products[productKeys[index]]
               
               var buyerEmail = await buyerRecord.email, sellerEmail = await sellerRecord.email;
@@ -54,11 +73,13 @@ module.exports = {
                 sellerEmail,
                 uri: item.uri,
                 deliveryStatus: item.deliveryStatus,
-
+                productName, 
+                productPrice,
+                productPostPrice,
               }
               // console.log(obj);
               await orders.push(obj);
-              console.log(orders)
+              // console.log(orders)
               if(orders.length == countOfOrders) {
                 res.render('index.ejs', {
                   title: "NottMyStyle Dashboard", orders
